69a72d2612d819715e063a651d0f98d77dab4865
hewish <v123vip@163.com>
2024-10-17 20:07:40 +0800

    添加macos15支持；

--- Sources/FullDiskAccess/FullDiskAccess.swift
+++ Sources/FullDiskAccess/FullDiskAccess.swift
@@ -15,10 +15,13 @@ public enum FullDiskAccess {
         case monterey // 12
         case ventura // 13
         case sonoma // 14
+        case sequoia // 15
     }
 
     private static var currentOS: MacOS {
-        if #available(macOS 14, *) {
+        if #available(macOS 15, *) {
+            return .sequoia
+        } else if #available(macOS 14, *) {
             return .sonoma
         } else if #available(macOS 13, *) {
             return .ventura
@@ -44,7 +47,7 @@ public enum FullDiskAccess {
         let checkPath: String
 
         switch currentOS {
-        case .monterey, .ventura, .sonoma:
+        case .monterey, .ventura, .sonoma, .sequoia:
             checkPath = "~/Library/Containers/com.apple.stocks"
         case .mojave, .catalina, .bigSur:
             checkPath = "~/Library/Safari"
